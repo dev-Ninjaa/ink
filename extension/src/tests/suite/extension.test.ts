d8eb74cc445bf20c825f0201f0cc2b212992b3d4
dev-Ninjaa <ayushsarkar357@gmail.com>
2026-08-29 13:29:31 +0100

    fix(extension): correct extension ID format in host presence test

--- extension/src/tests/suite/extension.test.ts
+++ extension/src/tests/suite/extension.test.ts
@@ -3,7 +3,7 @@ import * as vscode from "vscode";
 
 suite("Ink extension", () => {
   test("is present in the extension host", async () => {
-    const extension = vscode.extensions.getExtension("ninja75.ink server");
+    const extension = vscode.extensions.getExtension("ninja75.ink-server");
     assert.ok(extension);
   });
 });
